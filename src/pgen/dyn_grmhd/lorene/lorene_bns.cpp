5d9c16c413e86c64e849466118eb2fcff62e7ff4
PeterHammond <pph5189@psu.edu>
2026-02-24 06:17:46 +0100

    Add diagnostic output

--- src/pgen/dyn_grmhd/lorene/lorene_bns.cpp
+++ src/pgen/dyn_grmhd/lorene/lorene_bns.cpp
@@ -252,7 +252,7 @@ void SetupBNS(ParameterInput *pin, Mesh* pmy_mesh_) {
     } else {
       host_w0(m, IPR, k, j, i) = eos.template
                                 GetPFromRho<tov::LocationTag::Host>(rho);
-      std::cout<<"m,k,j,i,p:"<<m<<", "<<", "<<k<<", "<<j<<", "<<i<<", "<<host_w0(m, IPR, k, j, i)<<std::endl;
+      // std::cout<<"m,k,j,i,p:"<<m<<", "<<", "<<k<<", "<<j<<", "<<i<<", "<<host_w0(m, IPR, k, j, i)<<std::endl;
 
       // If the electron fraction is available, find it in the 1D EOS.
       if constexpr (use_ye) {
@@ -333,6 +333,7 @@ void SetupBNS(ParameterInput *pin, Mesh* pmy_mesh_) {
   if (pmbp->pz4c != nullptr) {
     Kokkos::deep_copy(pmbp->pz4c->u0, host_u_z4c);
   }
+  std::cout<<"m,k,j,i,w0(p):"<<3<<", "<<8<<", "<<47<<", "<<13<<", "<<w0(3, IPR, 8, 47, 13)<<std::endl;
 
   if (global_variable::my_rank == 0) {
     std::cout << "Data copied." << std::endl;
