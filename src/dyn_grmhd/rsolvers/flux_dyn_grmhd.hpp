aba7d8f28e2c23dca5275deeee1ef394f65a26a6
PeterHammond <pph5189@psu.edu>
2026-02-26 06:13:52 +0100

    Change SingleStateFlux to re-use scratch memory for LHS and RHS

--- src/dyn_grmhd/rsolvers/flux_dyn_grmhd.hpp
+++ src/dyn_grmhd/rsolvers/flux_dyn_grmhd.hpp
@@ -44,69 +44,70 @@ void SingleStateFlux(const PrimitiveSolverHydro<EOSPolicy, ErrorPolicy>& eos,
   eos.ps.PrimToCon(prim_l, cons_l, Bu_l, g3d);
   eos.ps.PrimToCon(prim_r, cons_r, Bu_r, g3d);
 
-  // Calculate W for the left state.
-  Real uul[3] = {prim_l[IVX], prim_l[IVY], prim_l[IVZ]};
-  Real udl[3];
-  Primitive::LowerVector(udl, uul, g3d);
-  Real iWsql = 1.0/(1.0 + Primitive::Contract(uul, udl));
-  Real iWl = sqrt(iWsql);
-  Real vcl = prim_l[pvx]*iWl - beta_u[ivx-IVX]*ialpha;
+  // Calculate W for the left state. 
+  // PH: These are only used per-side, can be re-used for rhs
+  Real uu_s[3] = {prim_l[IVX], prim_l[IVY], prim_l[IVZ]};
+  Real ud_s[3];
+  Primitive::LowerVector(ud_s, uu_s, g3d);
+  Real iWsq_s = 1.0/(1.0 + Primitive::Contract(uu_s, ud_s));
+  Real iW_s = sqrt(iWsq_s);
+  Real vc_s = prim_l[pvx]*iW_s - beta_u[ivx-IVX]*ialpha;
 
   // Calculate 4-magnetic field for the left state.
-  Real bul0 = Primitive::Contract(Bu_l, udl)*ialpha;
-  Real bdl[3], Bd_l[3];
-  Primitive::LowerVector(Bd_l, Bu_l, g3d);
+  // PH: Same as above
+  Real bu0_s = Primitive::Contract(Bu_l, ud_s)*ialpha;
+  Real bd_s[3], Bd_s[3];
+  Primitive::LowerVector(Bd_s, Bu_l, g3d);
   for (int a = 0; a < 3; a++) {
-    bdl[a] = (alpha*bul0*udl[a] + Bd_l[a])*iWl;
+    bd[a] = (alpha*bu0_s*ud_s[a] + Bd_s[a])*iW_s;
   }
-  bsql = (Primitive::SquareVector(Bu_l, g3d) + SQR(alpha*bul0))*iWsql;
+  bsql = (Primitive::SquareVector(Bu_l, g3d) + SQR(alpha*bu0_s))*iWsq_s;
 
   // Calculate fluxes for the left state.
-  flux_l[CDN] = cons_l[CDN]*vcl;
-  flux_l[CSX] = (cons_l[CSX]*vcl - bdl[0]*Bu_l[ibx]*iWl);
-  flux_l[CSY] = (cons_l[CSY]*vcl - bdl[1]*Bu_l[ibx]*iWl);
-  flux_l[CSZ] = (cons_l[CSZ]*vcl - bdl[2]*Bu_l[ibx]*iWl);
+  flux_l[CDN] = cons_l[CDN]*vc_s;
+  flux_l[CSX] = (cons_l[CSX]*vc_s - bd[0]*Bu_l[ibx]*iW_s);
+  flux_l[CSY] = (cons_l[CSY]*vc_s - bd[1]*Bu_l[ibx]*iW_s);
+  flux_l[CSZ] = (cons_l[CSZ]*vc_s - bd[2]*Bu_l[ibx]*iW_s);
   flux_l[csx] += (prim_l[PPR] + 0.5*bsql);
-  flux_l[CTA] = (cons_l[CTA]*vcl - alpha*bul0*Bu_l[ibx]*iWl
-          + (prim_l[PPR] + 0.5*bsql)*prim_l[ivx]*iWl);
+  flux_l[CTA] = (cons_l[CTA]*vc_s - alpha*bu0_s*Bu_l[ibx]*iW_s
+          + (prim_l[PPR] + 0.5*bsql)*prim_l[ivx]*iW_s);
 
   bflux_l[ibx] = 0.0;
-  bflux_l[iby] = (Bu_l[iby]*vcl -
-                    Bu_l[ibx]*(prim_l[pvy]*iWl - beta_u[pvy - PVX]*ialpha));
-  bflux_l[ibz] = (Bu_l[ibz]*vcl -
-                    Bu_l[ibx]*(prim_l[pvz]*iWl - beta_u[pvz - PVX]*ialpha));
+  bflux_l[iby] = (Bu_l[iby]*vc_s -
+                    Bu_l[ibx]*(prim_l[pvy]*iW_s - beta_u[pvy - PVX]*ialpha));
+  bflux_l[ibz] = (Bu_l[ibz]*vc_s -
+                    Bu_l[ibx]*(prim_l[pvz]*iW_s - beta_u[pvz - PVX]*ialpha));
 
   // Calculate W for the right state.
-  Real uur[3] = {prim_r[IVX], prim_r[IVY], prim_r[IVZ]};
-  Real udr[3];
-  Primitive::LowerVector(udr, uur, g3d);
-  Real iWsqr = 1.0/(1.0 + Primitive::Contract(uur, udr));
-  Real iWr = sqrt(iWsqr);
-  Real vcr = prim_r[pvx]*iWr - beta_u[ivx-IVX]*ialpha;
+  // PH: re-use temporaries from lhs
+  uu_s[0] = prim_r[IVX]; uu_s[1] = prim_r[IVY]; uu_s[2] = prim_r[IVZ];
+  Primitive::LowerVector(ud_s, uu_s, g3d);
+  iWsq_s = 1.0/(1.0 + Primitive::Contract(uu_s, ud_s));
+  iW_s = sqrt(iWsq_s);
+  vc_s = prim_r[pvx]*iW_s - beta_u[ivx-IVX]*ialpha;
 
   // Calculate 4-magnetic field for the right state.
-  Real bur0 = Primitive::Contract(Bu_r, udr)*ialpha;
-  Real bdr[3], Bd_r[3];
-  Primitive::LowerVector(Bd_r, Bu_r, g3d);
+  bu0_s = Primitive::Contract(Bu_r, ud_s)*ialpha;
+  Primitive::LowerVector(Bd_s, Bu_r, g3d);
   for (int a = 0; a < 3; a++) {
-    bdr[a] = (alpha*bur0*udr[a] + Bd_r[a])*iWr;
+    bd_s[a] = (alpha*bu0_s*ud_s[a] + Bd_s[a])*iW_s;
   }
-  bsqr = (Primitive::SquareVector(Bu_r, g3d) + SQR(alpha*bur0))*iWsqr;
+  bsqr = (Primitive::SquareVector(Bu_r, g3d) + SQR(alpha*bu0_s))*iWsq_s;
 
   // Calculate fluxes for the right state.
-  flux_r[CDN] = cons_r[CDN]*vcr;
-  flux_r[CSX] = (cons_r[CSX]*vcr - bdr[0]*Bu_r[ibx]*iWr);
-  flux_r[CSY] = (cons_r[CSY]*vcr - bdr[1]*Bu_r[ibx]*iWr);
-  flux_r[CSZ] = (cons_r[CSZ]*vcr - bdr[2]*Bu_r[ibx]*iWr);
+  flux_r[CDN] = cons_r[CDN]*vc_s;
+  flux_r[CSX] = (cons_r[CSX]*vc_s - bdr[0]*Bu_r[ibx]*iW_s);
+  flux_r[CSY] = (cons_r[CSY]*vc_s - bdr[1]*Bu_r[ibx]*iW_s);
+  flux_r[CSZ] = (cons_r[CSZ]*vc_s - bdr[2]*Bu_r[ibx]*iW_s);
   flux_r[csx] += (prim_r[PPR] + 0.5*bsqr);
-  flux_r[CTA] = (cons_r[CTA]*vcr - alpha*bur0*Bu_r[ibx]*iWr
-          + (prim_r[PPR] + 0.5*bsqr)*prim_r[ivx]*iWr);
+  flux_r[CTA] = (cons_r[CTA]*vc_s - alpha*bu0_s*Bu_r[ibx]*iW_s
+          + (prim_r[PPR] + 0.5*bsqr)*prim_r[ivx]*iW_s);
 
   bflux_r[ibx] = 0.0;
-  bflux_r[iby] = (Bu_r[iby]*vcr -
-                    Bu_r[ibx]*(prim_r[pvy]*iWr - beta_u[pvy - PVX]*ialpha));
-  bflux_r[ibz] = (Bu_r[ibz]*vcr -
-                    Bu_r[ibx]*(prim_r[pvz]*iWr - beta_u[pvz - PVX]*ialpha));
+  bflux_r[iby] = (Bu_r[iby]*vc_s -
+                    Bu_r[ibx]*(prim_r[pvy]*iW_s - beta_u[pvy - PVX]*ialpha));
+  bflux_r[ibz] = (Bu_r[ibz]*vc_s -
+                    Bu_r[ibx]*(prim_r[pvz]*iW_s - beta_u[pvz - PVX]*ialpha));
 }
 
 } // namespace dyngr
