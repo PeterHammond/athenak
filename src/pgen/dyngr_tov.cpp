346d372829accb8a95cdd17526fa7244df4477aa
PeterHammond <pph5189@psu.edu>
2026-03-10 16:46:36 +0100

    Revert "Add initialisation to mag-potential arrays"
    This reverts commit a7f1c8fdd1e5f4c8d9b923673d8911cf2b94a547.

--- src/pgen/dyngr_tov.cpp
+++ src/pgen/dyngr_tov.cpp
@@ -236,7 +236,7 @@ void SetupTOV(ParameterInput *pin, Mesh* pmy_mesh_) {
   std::cout << "ncellsx set." << std::endl;
   int nmb = pmbp->nmb_thispack;
   std::cout << "nmb set." << std::endl;
-  DvceArray4D<Real> a1("a1",1,1,1,1), a2("a2",1,1,1,1), a3("a3",1,1,1,1);
+  DvceArray4D<Real> a1, a2, a3;
   std::cout << "ax created." << std::endl;
   std::cout << nmb << ", " << ncells1 << ", "  << ncells2 << ", "  << ncells3 << std::endl;
   Kokkos::realloc(a1, nmb, ncells3, ncells2, ncells1);
